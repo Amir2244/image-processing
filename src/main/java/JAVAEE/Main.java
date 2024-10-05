29be8372d042dc06e4ebd2a88a79ed319b1257ed
amir.yosef <amir.yosef@hiast.edu.sy>
2024-10-05 22:12:27 +0300

    Setup main application

--- src/main/java/JAVAEE/Main.java
+++ src/main/java/JAVAEE/Main.java
@@ -1,7 +1,34 @@
 package JAVAEE;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+    public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+        BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+/*
+switch the strategy you like here
+*/
+        RecolorStrategy strategy = new BlockRecolorer();
+
+        long startTime = System.currentTimeMillis();
+/*
+change the number of threads here
+*/
+        strategy.recolor(originalImage, resultImage, 4);
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        File outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+
+        System.out.println("Duration: " + duration + " ms");
     }
 }
